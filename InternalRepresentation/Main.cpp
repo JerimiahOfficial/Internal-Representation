0fd6b0fb391b8a5967bbc1ea383d2dd3125796de
Jerimiah <38990407+JerimiahOfficial@users.noreply.github.com>
2021-10-30 17:16:46 -0400

    Update Main.cpp

--- InternalRepresentation/Main.cpp
+++ InternalRepresentation/Main.cpp
@@ -3,18 +3,23 @@
 #include <bitset>
 
 int main() {
+    // User prompt
     std::string arg;
     std::getline(std::cin, arg);
 
+    // Getting the binary equivalent of the input.
     std::bitset<8> Binary = std::bitset<8>(std::stoi(arg));
     std::bitset<8> Invert, Compliment;
 
+    // Invert the bitset to set it up for two's compliment.
     Invert = Binary;
     Invert.flip();
 
+    // Add one bit to the bit set to give the two's compliment.
     Compliment = Invert;
     Compliment = Compliment.to_ulong() + 1;
 
+    // Print out the information to the user.
     std::cout
         << "Entered Value: " + arg 
         << std::endl << std::endl
@@ -27,5 +32,6 @@ int main() {
         << "Compliment Decimal Value: " + std::to_string((int8_t)Compliment.to_ulong())
         << std::endl << std::endl;
 
+    // Pause so the user can see their information.
     system("pause");
 }
