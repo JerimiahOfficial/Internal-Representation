3957684a08d09c198f5fc094479580899c2fe7b2
Jerimiah <38990407+JerimiahOfficial@users.noreply.github.com>
2021-11-01 16:07:55 -0400

    Update Main.cpp

--- InternalRepresentation/Main.cpp
+++ InternalRepresentation/Main.cpp
@@ -7,10 +7,10 @@ int main() {
     std::string arg;
     std::getline(std::cin, arg);
 
-    // Converting the input into
-    std::bitset<8> Binary       = std::bitset<8>(std::stoi(arg));
-    std::bitset<8> Invert       = std::bitset<8>(~std::stoi(arg));
-    std::bitset<8> Compliment   = std::bitset<8>(~std::stoi(arg)).to_ulong() + 1ull;//Invert.to_ulong() + 1ull;
+    // Converting the input into Binary, One's Complement, and Two's Complement.
+    std::bitset<8> Binary   = std::bitset<8>(std::stoi(arg));
+    std::bitset<8> OneComp  = std::bitset<8>(~std::stoi(arg));
+    std::bitset<8> TwoComp  = std::bitset<8>(~std::stoi(arg)).to_ulong() + 1ull;//Invert.to_ulong() + 1ull;
 
     // IEEE conversion from float to 32-bit floating point.
     auto bitCast = std::bit_cast<uint32_t>(std::stof(arg));
@@ -19,9 +19,9 @@ int main() {
     // Print out the information to the user.
     std::cout   << "Entered Value: "        + arg
                 << "\n\nBinary Value: "     + Binary.to_string()
-                << "\nInverted Value: "     + Invert.to_string()
-                << "\n\nCompliment Value: " + Compliment.to_string()
-                << "\nCompliment Decimal: " + std::to_string((int8_t)Compliment.to_ulong())
+                << "\nInverted Value: "     + OneComp.to_string()
+                << "\n\nCompliment Value: " + TwoComp.to_string()
+                << "\nCompliment Decimal: " + std::to_string((int8_t)TwoComp.to_ulong())
                 << "\n\nIEEE Value: "       + IEEE.to_string() << std::endl;
 
     // Pause so the user can see their information.
